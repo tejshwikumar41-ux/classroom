ce55ef3c7a05c40f0d7dcf4acc0bdd86a13d41e3
tejshwikumar41-ux <tejshwikumar41-ux@users.noreply.github.com>
2026-04-15 16:20:12 +0530

    vercel theek kiya h humne

--- server.js
+++ server.js
@@ -372,6 +372,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'Login', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
